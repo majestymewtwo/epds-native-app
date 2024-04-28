14f5c996668402c35c39039ec393359ba2ad16fe
majestymewtwo <majestymewtwo@gmail.com>
2024-04-28 09:41:42 +0530

    setup pages and navigation

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { NativeWindStyleSheet } from "nativewind";
 import Home from "./src/screens/Home";
 import Login from "./src/screens/Login";
 import ValidateOTP from "./src/screens/OTP";
+import Products from "./src/screens/Products";
+import Cart from "./src/screens/Cart";
+import Orders from "./src/screens/Orders";
 
 NativeWindStyleSheet.setOutput({
   default: "native",
@@ -19,7 +22,7 @@ const navigationOptions = {
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{ animation: "none" }}>
         <Stack.Screen
           name='Login'
           options={navigationOptions}
@@ -34,6 +37,21 @@ export default function App() {
           name='Home'
           options={navigationOptions}
           component={Home}
+        />
+        <Stack.Screen
+          name='Products'
+          options={navigationOptions}
+          component={Products}
+        />
+        <Stack.Screen
+          name='Cart'
+          options={navigationOptions}
+          component={Cart}
+        />
+        <Stack.Screen
+          name='Orders'
+          options={navigationOptions}
+          component={Orders}
         />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ src/screens/Orders.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { ScrollView, Text, View } from "react-native";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const Orders = () => {
+  return (
+    <View className='flex-1 items-center justify-between bg-white min-h-screen'>
+      <Header />
+      <ScrollView className='p-2'>
+        {/* Add components here */}
+        <Text className='text-xl'>Orders Page</Text>
+      </ScrollView>
+      <Navbar />
+      <Footer />
+    </View>
+  );
+};
+
+export default Orders;
